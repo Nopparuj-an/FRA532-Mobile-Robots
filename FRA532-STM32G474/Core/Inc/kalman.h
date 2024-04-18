4774b06edb86ae645bc749bc4c02a5209f34111f
Nopparuj-an <47713359+Nopparuj-an@users.noreply.github.com>
2024-04-18 23:49:18 +0700

    update kalman

--- FRA532-STM32G474/Core/Inc/kalman.h
+++ FRA532-STM32G474/Core/Inc/kalman.h
@@ -11,7 +11,7 @@
 #include "main.h"
 #include "arm_math.h"
 
-typedef struct KalmanParams{
+typedef struct KalmanParams {
 	float32_t X_k[4];
 	float32_t P_k[16];
 	float32_t A[16];
@@ -40,9 +40,52 @@ typedef struct KalmanParams{
 	float32_t Es_velocity[1];
 	float32_t eye[16];
 	float32_t Z[1];
+
+	// Define initial state of X_pk and P_pk
+	arm_matrix_instance_f32 X_k_matrix;
+	arm_matrix_instance_f32 P_k_matrix;
+	// System matrices
+	arm_matrix_instance_f32 A_matrix;
+	arm_matrix_instance_f32 A_transpose_matrix;
+	arm_matrix_instance_f32 eye_matrix;
+	arm_matrix_instance_f32 B_matrix;
+	arm_matrix_instance_f32 C_matrix;
+	arm_matrix_instance_f32 C_transpose_matrix;
+	arm_matrix_instance_f32 G_matrix;
+	arm_matrix_instance_f32 G_transpose_matrix;
+	arm_matrix_instance_f32 Output_matrix;
+	arm_matrix_instance_f32 GGT_matrix;
+	arm_matrix_instance_f32 GQGT_matrix;
+	//------------------------------------------- for equa ------------------------------------
+
+	// Compute Xk = Ax + Bu
+	arm_matrix_instance_f32 Bu_matrix;
+	arm_matrix_instance_f32 Ax_matrix;
+
+	// Compute (C * P_k * C^T + R)
+	arm_matrix_instance_f32 CP_matrix;
+	arm_matrix_instance_f32 CPCT_matrix;
+	arm_matrix_instance_f32 CPCTR_matrix;
+
+	// Compute Kalman Gain: K = P_k * C^T * inv(C * P_k * C^T + R)
+	arm_matrix_instance_f32 K_matrix;
+	arm_matrix_instance_f32 PCT_matrix;
+
+	// Compute inverse of (C * P_k * C^T + R)
+	arm_matrix_instance_f32 CPCTRinv_matrix;
+
+	// Computation of the estimated state
+	arm_matrix_instance_f32 Cx_matrix;
+	arm_matrix_instance_f32 yCx_matrix;
+	arm_matrix_instance_f32 KyCx_matrix;
+
+	arm_matrix_instance_f32 R_matrix;
+	arm_matrix_instance_f32 Z_matrix;
+	arm_matrix_instance_f32 Velocity_matrix;
+	float Kalman_Speed;
 } KalmanFilter;
 
-float SteadyStateKalmanFilter(KalmanFilter* filter,float32_t Vin, float32_t Velocity);
-void Kalman_Start(KalmanFilter* filter);
+float SteadyStateKalmanFilter(KalmanFilter *filter, float32_t Vin, float32_t Velocity);
+void Kalman_Start(KalmanFilter *filter);
 
 #endif /* INC_KALMAN_H_ */
