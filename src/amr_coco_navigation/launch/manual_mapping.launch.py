0143c047139839fcc21513a7c975640c7899ddbf
Nopparuj-an <47713359+Nopparuj-an@users.noreply.github.com>
2024-04-24 21:52:52 +0700

    mapping usasble

--- src/amr_coco_navigation/launch/manual_mapping.launch.py
+++ src/amr_coco_navigation/launch/manual_mapping.launch.py
@@ -7,33 +7,44 @@ from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 
+
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    config_dir = os.path.join(get_package_share_directory('amr_coco_navigation'), 'config')
-    config_file = os.path.join(config_dir, 'mapper_params_online_sync.yaml')
+    use_sim_time = LaunchConfiguration("use_sim_time", default="true")
+    config_dir = os.path.join(
+        get_package_share_directory("amr_coco_navigation"), "config"
+    )
+    config_file = os.path.join(config_dir, "mapper_params_online_sync.yaml")
 
-    rviz_config_dir = os.path.join(config_dir, 'rviz')
-    rviz_config_file = os.path.join(rviz_config_dir, 'mapping.rviz')
+    rviz_config_dir = os.path.join(config_dir, "rviz")
+    rviz_config_file = os.path.join(rviz_config_dir, "mapping.rviz")
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
+    launch_description = LaunchDescription()
 
-        Node(
-            package='slam_toolbox',
-            executable='sync_slam_toolbox_node',
-            name='sync_slam_toolbox_node',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time}, config_file]),
+    declare_use_sim_time = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="true",
+        description="Use simulation (Gazebo) clock if true",
+    )
 
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='sync_slam_toolbox_node',
-            output='screen',
-            arguments=['-d', rviz_config_file],
-            parameters=[{'use_sim_time': use_sim_time}]),
-    ])
+    slam_toolbox = Node(
+        package="slam_toolbox",
+        executable="sync_slam_toolbox_node",
+        name="sync_slam_toolbox_node",
+        output="screen",
+        parameters=[{"use_sim_time": use_sim_time}, config_file],
+    )
 
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config_file],
+        parameters=[{"use_sim_time": use_sim_time}],
+    )
+
+    launch_description.add_action(declare_use_sim_time)
+    launch_description.add_action(slam_toolbox)
+    # launch_description.add_action(rviz)
+
+    return launch_description
